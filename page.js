c8161753582d4afad8de15dfe2fe5bf40fb7706b
trapzobinna <obinnaokeke15@yahoo.com>
2023-09-25 15:14:16 -0700

    Initial commit

---
+++ page.js
@@ -0,0 +1,30 @@
+'use strict';
+
+
+$(window).on('load', function() {
+	/*------------------
+		Preloader
+	--------------------*/
+	$(".loader").fadeOut(); 
+	$("#preloder").delay(400).fadeOut("slow");
+
+});
+(function($) {
+
+	/*------------------
+		Navigation
+	--------------------*/
+	$('.nav-switch').on('click', function(event) {
+		$('.main-menu').slideToggle(400);
+		event.preventDefault();
+	});
+
+
+	/*------------------
+		Background Set
+	--------------------*/
+	$('.set-bg').each(function() {
+		var bg = $(this).data('setbg');
+		$(this).css('background-image', 'url(' + bg + ')');
+
+	});
---
+++ trial.js
@@ -0,0 +1,12 @@
+
+	/*------------------
+		News Ticker
+	--------------------*/
+	$('.news-ticker').marquee({
+	    duration: 10000,
+	    //gap in pixels between the tickers
+	    //gap: 200,
+	    delayBeforeStart: 0,
+	    direction: 'left',
+	    duplicated: true
+	});
